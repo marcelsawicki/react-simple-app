ab4bc4ee78c28cca3403ce7b98002af39590c551
Marcel Sawicki <marcel.sawicki@gmail.com>
2022-07-01 12:01:51 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
+
+function Header({ title }) {
+  return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+
+
+function handleClick() {
+  
+}
+
 function App() {
   const value = 'World';
-  return <div>Hello {value} ⛵</div>;
+  return <div>
+    Hello {value} ⛵ - static app
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like (0)</button>
+    </div>
+    </div>;
 }
 
 export default App;
